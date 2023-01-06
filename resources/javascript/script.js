ba0ee3214eb9cb161b04261efde0e5a31e0fa3d4
Brian Quinde <bdqs_2747@hotmail.com>
2023-01-06 15:44:27 -0500

    only resume and some information

---
+++ resources/javascript/script.js
@@ -0,0 +1,35 @@
+
+const appButton = () => {
+    const gameobj=document.getElementById("game")
+    gameobj.style.height="300px";
+    document.getElementById("start").style.display="none";
+    gameobj.style.justifyContent="flex-start";
+    gameobj.style.alignItems="center";
+    document.getElementById("igapear").style.display="block";
+    const catobj = document.getElementById("cato")
+    const y = Math.floor(Math.random() * 100);
+    const x = Math.floor(Math.random() * 100);
+    catobj.style.display="block";
+    catobj.style.top= `${y}%`;
+    catobj.style.left= `${x}%`;
+}
+
+const catoaplast = () =>{
+    const texto = document.getElementById("igapear");
+    const catobj = document.getElementById("cato");
+    catobj.style.backgroundImage="url('./resources/images/catodeath.png')";
+    texto.innerHTML= "YOU WON!!";
+    const catoend = document.getElementById("end");
+    catoend.style.display="block";
+}
+
+
+const restart =()=>{
+    const gameobj=document.getElementById("game")
+    gameobj.style.height="auto";
+    document.getElementById("start").style.display="block";
+    document.getElementById("igapear").style.display="none";
+    const catobj = document.getElementById("cato")
+    catobj.style.display="none";
+    document.getElementById("end").style.display="none";
+}
